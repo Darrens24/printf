4b1322a069a35c37e6b1f23ac205a56b4b6a0056
Darrens24 <edouard.leleux@gmail.com>
2023-04-17 08:21:38 +0200

    le - a mettre en flag

--- bonus/main.c
+++ bonus/main.c
@@ -2,6 +2,11 @@
 
 int	main()
 {
-    printf("real:%-4u\n", -11);
-    ft_printf("mine:%-4u\n", -11);
+    printf("real:%04d\n", -14);
+    ft_printf("mine:%04d\n", -14);
 }
+
+/*
+flag 0:
+Le - doit s'afficher avant les zero, il faut donc print le - non pas dans les fonctions de print classique mais comme un left flag dans le cas ou les nombres sont signes il faut sans doute un nouveau booleen dans la structure.
+*/
